22030a142592b11f4dd136f9382b1826ab14469e
sowjanya27 <sowjanya.chekuri@gmail.com>
2014-01-26 16:11:35 -0800

    pytho script

---
+++ pythonscripts/example01.py
@@ -0,0 +1,8 @@
+#4.Match all the phone numbers in usa
+def phone_match(phone):
+	pat =re.compile('(^\([1-9][0-9][0-9]\)\\s?[0-9][0-9][0-9]\-[0-9][0-9][0-9][0-9])')
+	result = pat.match(phone)
+	print result 
+
+a = raw_input("Please, enter your  phone")#(408) 984-5561 or (408)984-5561
+phone_match(a)
