82f5b96347729bb2f5df89b05a18c7233c010e0b
sowjanya27 <sowjanya.chekuri@gmail.com>
2014-01-30 17:11:29 -0800

    Modified strings script version 1.1

---
+++ strings.py
@@ -0,0 +1,20 @@
+#string operations
+#
+
+#!/usr/bin/python
+a='hello'
+b="python"
+c='2'
+d=4.5
+f=a+' '+b
+print a;
+print b;
+print f
+#print d*b
+print a+b
+#print a*c
+#print c*b
+#rint c*d
+print c+b
+print a+c
+
