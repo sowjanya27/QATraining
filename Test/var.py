83b30af0ce148414071469e6fc5b38aa63d00924
sowjanya27 <sowjanya.chekuri@gmail.com>
2014-01-30 19:47:47 -0800

    Adding new variable file version 1.0

---
+++ Test/var.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+a=3
+b=5
+var1=var2=var3=1
+i,j,k=10,11.5,'hello'
+c=a+b
+d=a-b
+print i,j,k;
+#del var1,var2,var3
+print var1;print var2;print var3;
+
+print c;print d;
+print a*b
+print b/a
+print b%a
+
+
+
